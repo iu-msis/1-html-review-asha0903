53b3f31ad8aaa2503239142bf9585f989e720dea
asha0903 <57210527+asha0903@users.noreply.github.com>
2021-09-16 23:17:17 -0400

    Created js file for VueJs nad called API from randomuser

---
+++ app/public/js/index.js
@@ -0,0 +1,48 @@
+const app=Vue.createApp({
+    data() {
+        return{
+            "person":{},
+            "info":[
+                {
+                    "name": "Hope",
+                    "country":"USA",
+                    "birthday":"26 May",
+                    "age":"28",
+                    "email":"hope@example.com",
+                    "phone":"8976578779",
+                    "image": 'img/pic.jpeg'
+                    
+                },
+                {
+                    "name": "Ann",
+                    "country":"India",
+                    "birthday":"09 March",
+                    "age":"26",
+                    "email":"ann@example.com",
+                    "phone":"2342342343",
+                    "image": 'img/pic.jpeg'
+        
+                }
+            ]
+        }
+    },
+    
+created(){
+
+    fetch('https://randomuser.me/api/')
+    .then(response => response.json())
+    .then((responseJson) => {
+        console.log(responseJson);   //for debugging
+
+        this.person =responseJson.results[0];
+    })
+
+    .catch((err) => {
+        console.error(err);
+    })
+
+    }
+})
+
+
+// Vue.creteApp(app).mount('#offerApp')
